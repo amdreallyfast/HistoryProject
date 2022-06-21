10f0922b149c16f37757f53dba1e4d9546a606da
John Cox <coxjohn@microsoft.com>
2022-06-21 16:38:50 -0400

    Added connection to the Azure SQL database and retrieving all values from the Department table via a query.

---
+++ WebAPI/WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+            select *
+            from dbo.Department
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppConnectionStr");
+            SqlDataReader sqlDataReader;
+
+            //??simplify to single-line using statements? convert to Entity Framework??
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    table.Load(sqlDataReader);
+                    sqlDataReader.Close();
+                    sqlConnection.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
