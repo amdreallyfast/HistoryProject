78e257ead84b4d70536af41807823a12d44d53db
John Cox <coxjohn@microsoft.com>
2022-06-22 15:31:55 -0400

    Added EmployeeController. Fixed missing [Http[Get|Post]] attributes that were causing an error on launch: '500 : http://localhost:7218/swagger/v1/swagger.json'. After closing the windowindow, VS showed a semi-helpful stack trace that told me 'Actions require an explicit HttpMethod binding for Swagger/OpenAPI 3.0', and it told me which class that was. TIL.

--- WebAPI/WebAPI/Controllers/DepartmentController.cs
+++ WebAPI/WebAPI/Controllers/DepartmentController.cs
@@ -20,34 +20,35 @@ namespace WebAPI.Controllers
             _dataContext = dataContext;
         }
 
+        //[HttpGet]
+        //public JsonResult Get()
+        //{
+        //    string query = @"
+        //    select *
+        //    from dbo.Department
+        //    ";
+
+        //    DataTable table = new DataTable();
+        //    string sqlDataSource = _configuration.GetConnectionString("EmployeeAppConnectionStr");
+        //    SqlDataReader sqlDataReader;
+
+        //    //??simplify to single-line using statements? convert to Entity Framework??
+        //    using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+        //    {
+        //        sqlConnection.Open();
+        //        using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+        //        {
+        //            sqlDataReader = sqlCommand.ExecuteReader();
+        //            table.Load(sqlDataReader);
+        //            sqlDataReader.Close();
+        //            sqlConnection.Close();
+        //        }
+        //    }
+
+        //    return new JsonResult(table);
+        //}
+
         [HttpGet]
-        public JsonResult Get()
-        {
-            string query = @"
-            select *
-            from dbo.Department
-            ";
-
-            DataTable table = new DataTable();
-            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppConnectionStr");
-            SqlDataReader sqlDataReader;
-
-            //??simplify to single-line using statements? convert to Entity Framework??
-            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
-            {
-                sqlConnection.Open();
-                using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
-                {
-                    sqlDataReader = sqlCommand.ExecuteReader();
-                    table.Load(sqlDataReader);
-                    sqlDataReader.Close();
-                    sqlConnection.Close();
-                }
-            }
-
-            return new JsonResult(table);
-        }
-
         public async Task<ActionResult<List<Department>>> Get(int departmentId)
         {
             var departments = await _dataContext.Departments
@@ -57,6 +58,7 @@ namespace WebAPI.Controllers
             return departments;
         }
 
+        [HttpPost]
         public async Task<ActionResult<List<Department>>> Create(CreateDepartmentDto createDepartmentDto)
         {
             var newDepartment = new Department
---
+++ WebAPI/WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public EmployeeController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> Get(int employeeId)
+        {
+            var employees = await _dataContext.Employees
+                .Where(x => x.Id == employeeId)
+                .Include(x => x.Department)
+                .ToListAsync();
+            return employees;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Employee>>> Create(CreateEmployeeDto createEmployeeDto)
+        {
+            var department = await _dataContext.Departments
+                .FirstOrDefaultAsync(x => x.Id == createEmployeeDto.DepartmentId);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            var newEmployee = new Employee
+            {
+                Name = createEmployeeDto.Name,
+                DateOfJoining = createEmployeeDto.DateOfJoining,
+                PhotoFileName = createEmployeeDto.PhotoFileName,
+                Department = department
+            };
+
+            _dataContext.Employees.Add(newEmployee);
+            await _dataContext.SaveChangesAsync();
+            return await Get(newEmployee.Id);
+        }
+    }
+}
