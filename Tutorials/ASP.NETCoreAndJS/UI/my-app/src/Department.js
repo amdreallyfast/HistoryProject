d64e478ddbaf7c9fe05471a4572578ac7dfdc69a
amdreallyfast <amdreallyfast@hotmail.com>
2022-07-11 18:10:43 -0400

    Added a sorting function and buttons for sorting. Plus notes.

--- Tutorials/ASP.NETCoreAndJS/UI/my-app/src/Department.js
+++ Tutorials/ASP.NETCoreAndJS/UI/my-app/src/Department.js
@@ -61,6 +61,26 @@ export class Department extends Component {
         }, () => this.filterFunction())
     }
 
+    sortResult(property, ascending) {
+        var sortedData = this.state.departmentsWithoutFilter.sort(function (a, b) {
+            if (ascending) {
+                if (a[property] > b[property]) return 1
+                else if (a[property] < b[property]) return -1
+                else return 0
+            }
+            else {
+                if (b[property] > a[property]) return 1
+                else if (b[property] < a[property]) return -1
+                else return 0
+            }
+        })
+
+        this.setState({
+            ...this.state,
+            departments: sortedData
+        })
+    }
+
     changeNameFilter = (event) => {
         this.setState({
             ...this.state,
@@ -204,11 +224,41 @@ export class Department extends Component {
                     <thead>
                         <tr>
                             <th>
-                                <input className="form-control m-2" placeholder="Filter" onChange={this.changeIdFilter} />
+                                <div className="d-flex flex-row">
+                                    {/* Filter text box */}
+                                    <input className="form-control m-2" placeholder="Filter" onChange={this.changeIdFilter} />
+
+                                    {/* Sort up/down buttons */}
+                                    <button type="button" className="btn btn-light" onClick={() => this.sortResult("Id", true)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
+                                            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z" />
+                                        </svg>
+                                    </button>
+                                    <button type="button" className="btn btn-light" onClick={() => this.sortResult("Id", false)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
+                                            <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z" />
+                                        </svg>
+                                    </button>
+                                </div>
                                 DepartmentId
                             </th>
                             <th>
-                                <input className="form-control m-2" placeholder="Filter" onChange={this.changeNameFilter} />
+                                <div className="d-flex flex-row">
+                                    {/* Filter text box */}
+                                    <input className="form-control m-2" placeholder="Filter" onChange={this.changeNameFilter} />
+
+                                    {/* Sort up/down buttons */}
+                                    <button type="button" className="btn btn-light" onClick={() => this.sortResult("Name", true)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
+                                            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z" />
+                                        </svg>
+                                    </button>
+                                    <button type="button" className="btn btn-light" onClick={() => this.sortResult("Name", false)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
+                                            <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z" />
+                                        </svg>
+                                    </button>
+                                </div>
                                 DepartmentName
                             </th>
                             <th>Options</th>
