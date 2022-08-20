5124e634b2486f47c9049beddcb2614a730a36ad
amdreallyfast <amdreallyfast@hotmail.com>
2022-08-20 11:04:58 -0400

    Starting to add components and stub out the layout. But it's not compiling, saying, "Module not found: Error: Can't resolve 'react-router-dom' in 'C:\GitHub\HistoryProject\npmfrontend\src'"

--- npmfrontend/src/App.js
+++ npmfrontend/src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+// import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
+import { Home } from './Components/Home'
+import { About } from './Components/About'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <h3 className="d-flext justify-content-center m-3">
+                    History Project
+                </h3>
+
+                <nav className='navbar navbar-expand-sm bg-light navbar-dark'>
+                    <ul className='navbar-nav'>
+                        <li className='nav-item- m-1'>
+                            <NavLink className='btn btn-light btn-outline-primary' to='/home'>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className='nav-item- m-1'>
+                            <NavLink className='btn btn-light btn-outline-primary' to='/about'>
+                                About
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ npmfrontend/src/Components/Home/DisplayEntry.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export class DisplayEntry extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    refreshList() {
+        // TODO: something like this"
+        // fetch(variables.API_URL + "Department/GetAll")
+        // .then(response => response.json())
+        // .then(data => {
+        //     this.setState({
+        //         departments: data,
+        //         departmentsWithoutFilter: data
+        //     })
+        // })
+    }
+
+    render() {
+        return (
+            <div>
+                Hello, I'm DisplayEntry
+            </div>
+        )
+    }
+}
