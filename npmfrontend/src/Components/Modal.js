21f08ad6dd2a7b7608214c031bef212ad0da0a0d
amdreallyfast <amdreallyfast@hotmail.com>
2022-09-23 09:40:54 -0400

    Refining the About modal

--- npmfrontend/src/Components/Modal.js
+++ npmfrontend/src/Components/Modal.js
@@ -5,10 +5,13 @@ export default function Modal(props) {
     const toggleFunc = props.onClickAction
 
     return (
-        <div className={`${styles.container} ${modalState ? styles.active : ''}`}>
+        <div className={`${styles.modalcontainer} ${modalState ? styles.active : ''}`}>
             <div className={styles.modal}>
-                TODO: Modal content
-                <div className={styles.close} onClick={toggleFunc}></div>
+                <div className={styles.textblock}>
+                    <p><b>History of the World</b></p>
+                    <p>This project sees the potential in gathering many perspectives on a variety of events through time. With enough viewpoints, a greater understanding is possible.<br/>- John Cox, creator, Microsoft Hackathon 9/23/2022</p>
+                </div>
+                <div className={styles.closeicon} onClick={toggleFunc}></div>
             </div>
         </div>
     )
