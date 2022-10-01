05dfbd33cc8f4c10c2009d7a73a717bb6d9807b1
amdreallyfast <amdreallyfast@hotmail.com>
2022-10-01 09:32:54 -0400

    Removing unused code

--- npmfrontend/src/App.js
+++ npmfrontend/src/App.js
@@ -8,28 +8,6 @@ import { DisplayEntry } from './Components/DisplayEntry'
 import { Timeline } from './Components/Timeline'
 import { Home } from './Components/Home';
 
-// Reference:
-//  React Cards with Props | UI Card Design with React JS
-//  https://www.youtube.com/watch?v=4KxHcbQ8GYQ
-function Card() {
-  return (
-    <div className='card'>
-      <div className='card__body'>
-        <img src='' />
-        <h2 className='card__title'>
-          A fabulous title
-        </h2>
-        <p className='card__description'>
-          This is the card description
-        </p>
-      </div>
-      <button className='card__btn'>
-        View Recipe
-      </button>
-    </div>
-  );
-}
-
 export default function App() {
   const [aboutModalState, setModalState] = React.useState(false)
 
@@ -43,19 +21,6 @@ export default function App() {
         <h3 className="d-flext justify-content-center m-3">
           History Project
         </h3>
-
-        {/* <nav className='navbar navbar-expand-sm bg-light navbar-dark'>
-          <NavLink className='btn btn-light btn-outline-primary' to='/about'>
-            About
-          </NavLink>
-        </nav>
-
-        <Routes>
-          <Route path='/about' element={<About />} />
-        </Routes>
-
-        <Home /> */}
-
         <span onClick={toggleModalState} className="aboutlinktext">
           About (Modal state: '{aboutModalState.toString()}')
         </span>
