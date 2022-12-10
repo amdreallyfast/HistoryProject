1adda67c5355949443e31bca844fcaedbfdce2a2
amdreallyfast <amdreallyfast@hotmail.com>
2022-12-10 12:52:15 -0500

    Whoops, forgot to restore that line after all my debugging.

--- WebAPI/WebAPI/Models/Event.cs
+++ WebAPI/WebAPI/Models/Event.cs
@@ -5,6 +5,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string ImageFilePath { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public DateTime LowerTimeBoundary { get; set; } = DateTime.Now;
         public DateTime UpperTimeBoundary { get; set; } = DateTime.MinValue;
