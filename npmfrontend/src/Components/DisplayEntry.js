5564ccb9783eab50b4a3cc03a652c3e13a6516e6
amdreallyfast <amdreallyfast@hotmail.com>
2022-12-30 15:18:47 -0500

    (finally) got (most of) the info rendering for the loaded event

--- npmfrontend/src/Components/DisplayEntry.js
+++ npmfrontend/src/Components/DisplayEntry.js
@@ -13,9 +13,25 @@ export class DisplayEntry extends Component {
     this.state = {
       editMode: false,
 
+      titleMinLen: 10,
+      titleMaxLen: 128,
+      imageFilePathMaxLen: 256,
+      summaryTextMinLen: 50,
+      summaryTextMaxLen: 2048,
+
       selectImageModalVisible: false,
-      imageUploadFailedMsg: "",
       defaultImagePath: variables.PHOTO_URL + "anonymous.png",
+
+      // TODO: switch "" -> null; null would be preferred rather than requiring the generation of a new string object
+      // TODO: also switch 0 -> null because some time elements ( hour, min) can be 0. "Missing data" should be null.
+      errMsgs: {
+        eventTitleErrMsg: "title error message",
+        eventImgErrMsg: "image error message",
+        eventSummaryErrMsg: "summary error message",
+        eventWhenErrMsg: "when error message",
+        eventRegionErrMsg: "region error message",
+        eventRevisionInfoErrMsg: "revision info error message",
+      },
 
       event: {
         titleText: "",
@@ -27,14 +43,14 @@ export class DisplayEntry extends Component {
             Month: 0,
             Day: 0,
             Hour: 0,
-            Sec: 0,
+            Min: 0,
           },
           upperBound: {
             Year: 0,
             Month: 0,
             Day: 0,
             Hour: 0,
-            Sec: 0,
+            Mon: 0,
           }
         },
         region: [],
@@ -45,6 +61,9 @@ export class DisplayEntry extends Component {
   }
 
   setEvent = (eventJson) => {
+    console.log("setting event:");
+    console.log(eventJson);
+
     this.setState({
       ...this.state,
       event: {
@@ -80,19 +99,82 @@ export class DisplayEntry extends Component {
       }
     });
 
-    this.displayWhereContainer();
+    console.log(`title: '${this.state.event.titleText}'`)
+    // this.generateWhereHtml();
+    // this.generateWhenHtml();
+    // this.generateSummaryHtml();
+  }
+
+  checkEventForErrors = (eventJson) => {
+    console.log("checkEventForErrors:");
+    console.log(eventJson);
+
+    // Title
+    let titleErrMsg = null;
+    if (eventJson.Title.length === 0) {
+      titleErrMsg = `Must be at least '${this.state.titleMinLen}' characters.`;
+    }
+    else if (eventJson.Title.length > this.state.titleMaxLen) {
+      titleErrMsg = `Must be less than '${this.state.titleMaxLen}' characters.`;
+    }
+
+    // Image file path
+    let imgErrMsg = null;
+    if (eventJson.ImageFilePath.length > this.state.imageFilePathMaxLen) {
+      imgErrMsg = `Must be less than '${this.state.imageFilePathMaxLen} characters.`;
+    }
+
+    // Summary
+    let summaryErrMsg = null;
+    if (eventJson.Summary.length < this.state.summaryTextMinLen) {
+      summaryErrMsg = `Must be at least '${this.state.summaryTextMinLen}' characters.`;
+    }
+    else if (eventJson.Summary.length > this.state.summaryTextMaxLen) {
+      summaryErrMsg = `Must be at less than '${this.state.summaryTextMaxLen}' characters.`;
+    }
+
+    // Time range
+    let whenErrMsg = null;
+    if (eventJson.TimeRange.LowerBoundYear === null || eventJson.TimeRange.UpperBoundYear === null) {
+      whenErrMsg = "Must select both upper and lower time boundaries.";
+    }
+
+    // Region
+    let whereErrMsg = null;
+    if (eventJson.Region.length === 0) {
+      whereErrMsg = "Must select at least one location.";
+    }
+
+    // Set the error message.    
+    this.setState({
+      // ...this.state,
+      errMsgs: {
+        ...this.state.errMsgs,
+        eventTitleErrMsg: titleErrMsg,
+        eventImgErrMsg: imgErrMsg,
+        eventSummaryErrMsg: summaryErrMsg,
+        eventWhenErrMsg: whenErrMsg,
+        eventRegionErrMsg: whereErrMsg,
+      }
+    });
   }
 
   getSelectedEvent = (eventId) => {
     fetch(`${variables.API_URL}Event/Get/${eventId}`)
       .then(response => response.json())
-      .then(eventJson => this.setEvent(eventJson));
+      .then(eventJson => {
+        this.setEvent(eventJson);
+        this.checkEventForErrors(eventJson);
+      });
   }
 
   getEventOfTheDay = () => {
     fetch(`${variables.API_URL}Event/GetEventOfTheDay`)
       .then(response => response.json())
-      .then(eventJson => this.setEvent(eventJson));
+      .then(eventJson => {
+        this.setEvent(eventJson);
+        this.checkEventForErrors(eventJson);
+      });
     console.log(this.state)
   }
 
@@ -123,13 +205,64 @@ export class DisplayEntry extends Component {
   cancelChanges = () => {
     // TODO: this
 
-    //??just re-fetch??
+    //??just re-fetch? keep a backup event state from when it was loaded and then restore that??
     console.log("cancelling changes")
     console.log("turning off edit mode")
     this.setState({
       ...this.state,
       editMode: false
     })
+  }
+
+
+  showMyModal = () => {
+    // Set state to show the modal.
+    console.log("showMyModal")
+    this.setState({
+      ...this.state,
+      selectImageModalVisible: true,
+    });
+  }
+
+  onTitleChanged = (changeEvent) => {
+    console.log("onTitleChanged");
+    this.setState({
+      ...this.state,
+      event: {
+        ...this.state.event,
+        titleText: changeEvent.target.value
+      }
+    });
+  }
+
+  titleHtml = () => {
+    console.log("titleHtml:");
+
+    return (
+      <>
+        {/* Data */}
+        <div className="display-title-text-container">
+          {this.state.editMode === true ?
+            <input type="text"
+              className="form-control"
+              value={this.state.event.titleText}
+              onChange={this.onTitleChanged}>
+            </input>
+            :
+            <span>{this.state.event.titleText}</span>
+          }
+        </div>
+
+        {/* Error */}
+        {this.state.errMsgs.eventTitleErrMsg === "" ?
+          null
+          :
+          <div className="display-title-err-container">
+            <span className="err-msg">{this.state.errMsgs.eventTitleErrMsg}</span>
+          </div>
+        }
+      </>
+    );
   }
 
   // Thanks for react-bootstrap for this demo code. I've modified it to work in classes (replace 
@@ -176,41 +309,162 @@ export class DisplayEntry extends Component {
         </Modal>
       </>
     );
-
   }
 
-  showMyModal = () => {
-    // Set state to show the modal.
-    console.log("showMyModal")
-    this.setState({
-      ...this.state,
-      selectImageModalVisible: true,
-    });
+  imageHtml = () => {
+    console.log("imageHtml:")
+
+    return (
+      <>
+        {/* Data */}
+        {this.state.event.imageFilePath === "" ?
+          <span>No image</span>
+          :
+          <>
+            <img
+              className="event-image"
+              src={variables.PHOTO_URL + this.state.event.imageFilePath}
+              alt="If you're seeing this, then the event could not be loaded. BADBADBADPANIC!!">
+            </img>
+
+            {this.state.editMode === true ?
+              <Button variant="primary" className="imageEditButtonselectEventImageBtn" onClick={this.showMyModal}>
+                Select image
+              </Button>
+              :
+              null
+            }
+            <this.editEventImgModal />
+          </>
+        }
+
+        {/* Error */}
+        {this.state.errMsgs.eventImgErrMsg === "" ?
+          null :
+          <div className="event-image-err-container">
+            <span className="err-msg">{this.state.errMsgs.eventImgErrMsg}</span>
+          </div>
+        }
+      </>
+    );
   }
 
-  onTitleChanged = (changeEvent) => {
-    console.log("onTitleChanged");
-    this.setState({
-      ...this.state,
-      event: {
-        ...this.state.event,
-        titleText: changeEvent.target.value
-      }
-    });
+  regionHtml = () => {
+    console.log("regionHtml:");
+    return (
+      <>
+        <div>
+          {this.state.event.region.map((latLong) => (
+            <div>
+              <span>'{latLong.lat}', '{latLong.long}'</span>
+            </div>
+          ))}
+        </div>
+
+        {/* TODO: remove the trinary operator and just use the div and span. If contents are null, then they will not be visible when the HTML renders. */}
+        {this.state.errMsgs.eventRegionErrMsg === null ?
+          null
+          :
+          <div className="event-where-err-container">
+            <span className="err-msg">
+              {this.state.errMsgs.eventRegionErrMsg}
+            </span>
+          </div>
+        }
+      </>
+    );
   }
 
-  displayWhereContainer = () => {
-    console.log("displayWhereContainer:");
-    let htmlString = "";
-    this.state.event.region.forEach(latLong => {
-      htmlString += `<div>'${latLong.lat}', '${latLong.long}'</div>\n`;
-    });
-    console.log("htmlString:");
-    console.log(htmlString);
+  timePrettyStr = (year, month, day, hour, min) => {
+    let dateStr = null;
+    if (year !== null && month !== null && day !== null) {
+      dateStr = `${year}/${month}/${day}`;
+    }
+    else if (year !== null && month !== null) {
+      dateStr = `${year}/${month}`;
+    }
+    else {
+      // Note: Year should never be null, so don't bother to check.
+      // Also Note: Don't bother checking the combination if year != null, month == null, and 
+      // day != null. Having a day without a month would be very silly.
+      dateStr = `${year}`
+    }
 
-    const whereElement = document.querySelector("#whereContainer");
-    whereElement.innerHTML = htmlString;
+    let timeWithLeadingZeros = (num) => {
+      return num < 10 ? `0${num}` : `${num}`
+    };
+
+    let timeStr = null;
+    if (hour !== null && min !== null) {
+      timeStr = `${timeWithLeadingZeros(hour)}:${timeWithLeadingZeros(min)}`
+    }
+    else if (hour !== null) {
+      timeStr = `${timeWithLeadingZeros(hour)}`
+    }
+
+    // console.log(`dateStr: '${dateStr}', timeStr: '${timeStr}'`);
+    return timeStr === null ? dateStr : `${dateStr} ${timeStr}`
   }
+
+  timeRangeHtml = () => {
+    console.log("timeRangeHtml:");
+
+    let lowerBound = this.state.event.timeRange.lowerBound;
+    let lowerBoundText = this.timePrettyStr(lowerBound.Year, lowerBound.Month, lowerBound.Day, lowerBound.Hour, lowerBound.Min);
+    console.log(`lowerBoundText: '${lowerBoundText}'`);
+
+    let upperBound = this.state.event.timeRange.upperBound;
+    let upperBoundText = this.timePrettyStr(upperBound.Year, upperBound.Month, upperBound.Day, upperBound.Hour, upperBound.Min);
+    console.log(`upperBoundText: '${upperBoundText}'`);
+
+    return (
+      <>
+        <div>
+          {/* TODO: make small text, like a header note */}
+          <span>
+            Year/month/day hour:min
+          </span>
+        </div>
+        <span>{lowerBoundText}</span>
+        <span style={{ float: "right" }}>{upperBoundText}</span>
+        {this.state.errMsgs.eventWhenErrMsg === null ?
+          null
+          :
+          <div className="event-when-err-container">
+            <span className="err-msg">
+              {this.state.errMsgs.eventWhenErrMsg}
+            </span>
+          </div>
+        }
+      </>
+    );
+  }
+
+  summaryHtml = () => {
+    console.log("generateSummaryHtml:");
+
+    return (
+      <>
+        <div>
+          <span>{this.state.event.summaryText}</span>
+        </div>
+
+        <div className="event-summary-char-count-container">
+          <span>{this.state.event.summaryText.length}/{this.state.summaryTextMaxLen}</span>
+        </div>
+
+        {this.state.errMsgs.eventSummaryErrMsg === null ?
+          null :
+          <div className="event-summary-err-container">
+            <span className="err-msg">
+              {this.state.errMsgs.eventSummaryErrMsg}
+            </span>
+          </div>
+        }
+      </>
+    );
+  }
+
 
   render() {
     console.log("rendering");
@@ -220,57 +474,42 @@ export class DisplayEntry extends Component {
             If in edit mode, use an input form, otherwise use a non-editable span. 
         */}
         <div className="display-title-container">
-          {this.state.editMode === true ?
-            <input type="text"
-              className="form-control"
-              value={this.state.event.titleText}
-              onChange={this.onTitleChanged}>
-            </input>
-            :
-            <span>{this.state.event.titleText}</span>
-          }
+          <this.titleHtml />
         </div>
 
         {/* Image 
             Note: If no image, just say so.
         */}
         <div className="display-image-container">
-          {this.state.event.imageFilePath === "" ?
-            <span>No image</span>
-            :
-            <>
-              <img
-                className="eventImg"
-                src={variables.PHOTO_URL + this.state.event.imageFilePath}
-                alt="If you're seeing this, then the event could not be loaded. BADBADBADPANIC!!">
-              </img>
-
-              {this.state.editMode === true ?
-                <Button variant="primary" className="imageEditButtonselectEventImageBtn" onClick={this.showMyModal}>
-                  Select image
-                </Button>
-                :
-                null
-              }
-              <this.editEventImgModal />
-            </>
-          }
+          <this.imageHtml />
         </div>
 
-        {/* TODO: create a list of (lat,lon) pairs */}
-        <div id="whereContainer" className="display-where-container">
-          <span>Where</span>
+        {/* Region
+        */}
+        <div className="display-where-container">
+          <this.regionHtml />
         </div>
+
+        {/* TODO: move "when" to just below title */}
+        {/* When
+        */}
         <div className="display-when-container">
-          <span>When</span>
-        </div>
-        <div className="display-summary-container">
-          <span>{this.state.summaryText}</span>
+          <this.timeRangeHtml />
         </div>
 
+        {/* Summary */}
+        <div className="display-summary-container">
+          <this.summaryHtml />
+        </div>
+
+        {/* Edit/(Submit/Cancel) */}
+        {/* TODO: let "escape" key also cancel edit mode */}
         {this.state.editMode === true ?
           <>
-            {/* "Cancel" goes on the far right, and then put "submit" next to it. */}
+            {/* "Cancel" goes on the far right, and then put "Submit" next, also on the far 
+            right, but since the "Cancel" button is already there, "Summary" will end up 
+            immediately left of it.
+            */}
             <button type="button" className="btn btn-secondary float-end" onClick={this.cancelChanges}>
               Cancel
             </button>
