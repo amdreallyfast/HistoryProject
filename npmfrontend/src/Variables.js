5759d73b27770aa7d0b315ba6614a3eed4f8d9a5
amdreallyfast <amdreallyfast@hotmail.com>
2023-09-12 12:57:16 -0400

    Refactor: Removing reactjs code

--- npmfrontend/src/Variables.js
+++
@@ -1,8 +0,0 @@
-// Note: See C:\GitHub\HistoryProject\WebAPI\WebAPI\Properties\launchSettings.json 
-//  -> "profiles": "WebAPI": "applicationUrl" for first choice of "localhost" that will be used 
-// when the application is launched.
-export const variables = {
-  HOST: "https://localhost:7121",
-  // API_URL: "https://localhost:7121/api",
-  // PHOTO_URL: "https://localhost:7121/Photos"
-}
