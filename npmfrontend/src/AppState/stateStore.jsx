be89a269d27ca39a4da63cec37a8e0da1c19433c
amdreallyfast <amdreallyfast@hotmail.com>
2023-11-04 16:42:19 -0400

    Cleanup: Removing unused files.

--- npmfrontend/src/AppState/stateStore.jsx
+++ npmfrontend/src/AppState/stateStore.jsx
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { stateSlicePoi } from "./stateSlicePoi";
 import { stateSliceEditPoi } from "./stateSliceEditPoi";
-import { stateSliceSearch } from "./stateSliceSearch";
 
 export const stateStore = configureStore({
   reducer: {
     poiReducer: stateSlicePoi.reducer,
-    editPoiReducer: stateSliceEditPoi.reducer,
-    searchReducer: stateSliceSearch.reducer
+    editPoiReducer: stateSliceEditPoi.reducer
   }
 })
