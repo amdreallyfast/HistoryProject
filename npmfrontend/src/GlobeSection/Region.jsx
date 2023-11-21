5d2184f7bceddab9b7d634353cd0dec3c9c6851f
amdreallyfast <amdreallyfast@hotmail.com>
2023-11-21 07:18:21 -0500

    Experiment: Trying "d3-geo-voronoi" to again to see if maybe I'm just using the "delaunator" library incorrectly.
    Nope. Same behavior.

--- npmfrontend/src/GlobeSection/Region.jsx
+++ npmfrontend/src/GlobeSection/Region.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react"
 import * as THREE from "three"
 import Delaunator from "delaunator"
+import * as d3Geo from "d3-geo-voronoi"
 
 function ConvertLatLongToXYZ(lat, long, sphereRadius) {
   let latRad = (lat / 180.0) * Math.PI
@@ -85,9 +86,13 @@ export function Region() {
     })
 
     // let expectedIndicesArr = [24, 21, 25, 21, 22, 25, 24, 26, 21, 26, 23, 21, 18, 19, 22, 28, 27, 24, 24, 27, 26, 25, 28, 24, 18, 22, 21, 22, 19, 25, 25, 19, 28, 23, 18, 21, 27, 29, 26, 26, 29, 23, 30, 29, 27, 23, 20, 18, 29, 20, 23, 28, 30, 27, 28, 31, 30, 19, 31, 28, 18, 16, 19, 15, 16, 18, 20, 15, 18, 35, 32, 30, 30, 32, 29, 29, 17, 20, 20, 17, 15, 53, 17, 29, 31, 33, 30, 37, 34, 31, 31, 34, 33, 16, 13, 19, 15, 13, 16, 12, 13, 15, 17, 12, 15, 33, 35, 30, 17, 14, 12, 12, 10, 13, 34, 37, 33, 33, 36, 35, 19, 37, 31, 37, 36, 33, 13, 10, 19, 9, 10, 12, 14, 9, 12, 36, 38, 35, 35, 38, 32, 17, 11, 14, 14, 11, 9, 9, 7, 10, 37, 39, 36, 36, 41, 38, 6, 7, 9, 19, 40, 37, 37, 40, 39, 11, 6, 9, 44, 41, 39, 39, 41, 36, 11, 8, 6, 40, 43, 39, 43, 42, 39, 3, 4, 6, 6, 4, 7, 7, 4, 10, 8, 3, 6, 42, 44, 39, 41, 44, 38, 38, 53, 32, 32, 53, 29, 11, 5, 8, 8, 5, 3, 3, 1, 4, 43, 46, 42, 42, 47, 44, 40, 46, 43, 0, 1, 3, 46, 45, 42, 5, 0, 3, 45, 47, 42, 5, 2, 0, 46, 49, 45, 45, 50, 47, 40, 49, 46, 49, 48, 45, 48, 50, 45, 47, 50, 44, 44, 53, 38, 52, 51, 48, 48, 51, 50, 49, 52, 48, 52, 53, 51, 51, 53, 50, 50, 53, 44, 17, 53, 11]
-    const testTypedArr = new Float32Array(testLongLatArr.flat())
-    let delaunator = new Delaunator(testTypedArr)
-    let indicesArr = delaunator.triangles
+    // const testTypedArr = new Float32Array(testLongLatArr.flat())
+    // let delaunator = new Delaunator(testTypedArr)
+    // let indicesArr = delaunator.triangles
+    let delaunator = new d3Geo.geoDelaunay(testLongLatArr)
+    let indicesArr = delaunator.triangles.flat()
+    console.log({ edges: delaunator.edges })
+    // TODO: go through all index pairs of vertices and remove pairs that are longer than 3
 
     // Create geometry for the points
     let valuesPerVertex = 3
