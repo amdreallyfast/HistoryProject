e1074b8c59f8ac725b73a72dc995683f1b90ba5b
amdreallyfast <amdreallyfast@hotmail.com>
2023-12-02 17:07:50 -0500

    Experiment: Failing to interpolate between vertices on a triangle. I am so stumped.

--- npmfrontend/src/GlobeSection/Region.jsx
+++ npmfrontend/src/GlobeSection/Region.jsx
@@ -7,17 +7,23 @@ import { LatLongPin } from "./LatLongPin"
 // import Delaunator from "delaunator"
 import * as d3Geo from "d3-geo-voronoi"
 import { sum } from "lodash"
+import { v1 } from "uuid"
 
 // TODO: latlong grid to fill in gaps >1deg
 // TODO: latlong pin object
 // TODO: click latlong pin and drag to move point
 
-const interpolateArc = (startLat, startLong, endLat, endLong, interpolationAngleDeg) => {
+const interpolateArcFromLatLong = (startLat, startLong, endLat, endLong, interpolationAngleDeg) => {
   const [startX, startY, startZ] = ConvertLatLongToXYZ(startLat, startLong, globeInfo.regionRadius)
-  const [endX, endY, endZ] = ConvertLatLongToXYZ(endLat, endLong, globeInfo.regionRadius)
+  let start = new THREE.Vector3(startX, startY, startZ)
 
-  let startVec3 = new THREE.Vector3(startX, startY, startZ)
-  let endVec3 = new THREE.Vector3(endX, endY, endZ)
+  const [endX, endY, endZ] = ConvertLatLongToXYZ(endLat, endLong, globeInfo.regionRadius)
+  let end = new THREE.Vector3(endX, endY, endZ)
+
+  return interpolateArcFromVertices(start, end, interpolationAngleDeg)
+}
+
+const interpolateArcFromVertices = (startVec3, endVec3, interpolationAngleDeg) => {
   // console.log({ start: startVec3, end: endVec3 })
 
   let unitStartVec3 = startVec3.clone().normalize()
@@ -44,13 +50,32 @@ const interpolateArc = (startLat, startLong, endLat, endLong, interpolationAngle
   return interpolatedPoints
 }
 
+// TODO: ??re-think this algorithm? it doesn't work well with triangles; do we even need it??
 const findMidpointOnSurface = (latLongArr) => {
   // create average point
   let minX = 0, maxX = 0
   let minY = 0, maxY = 0
   let minZ = 0, maxZ = 0
   latLongArr.forEach((latLongJson, index) => {
+
+
     const [x, y, z] = ConvertLatLongToXYZ(latLongJson.lat, latLongJson.long, globeInfo.radius)
+    // console.log({
+    //   x: x,
+    //   y: y,
+    //   z: z
+    // })
+
+    // const [tempLat, tempLong] = ConvertXYZToLatLong(x, y, z, globeInfo.radius)
+    // const [tempX, tempY, tempZ] = ConvertLatLongToXYZ(tempLat, tempLong, globeInfo.radius)
+    // console.log({
+    //   x1: x,
+    //   x2: tempX,
+    //   y1: y,
+    //   y2: tempY,
+    //   z1: z,
+    //   z2: tempZ
+    // })
 
     if (index == 0) {
       // first loop; assign min/max
@@ -85,10 +110,23 @@ const findMidpointOnSurface = (latLongArr) => {
     }
   })
 
-  let midpointX = (minX + maxX) * 0.5
-  let midpointY = (minY + maxY) * 0.5
-  let midpointZ = (minZ + maxZ) * 0.5
-  let midpoint = new THREE.Vector3(midpointX, midpointY, midpointZ)
+  let midX = (minX + maxX) * 0.5
+  let midY = (minY + maxY) * 0.5
+  let midZ = (minZ + maxZ) * 0.5
+
+  // console.log({
+  //   x_min: minX,
+  //   x_max: maxX,
+  //   x_mid: midX,
+  //   y_min: minY,
+  //   y_max: maxY,
+  //   y_mid: midY,
+  //   z_min: minZ,
+  //   z_max: maxZ,
+  //   z_mid: midZ,
+  // })
+
+  let midpoint = new THREE.Vector3(midX, midY, midZ)
   let midpointVector = (new THREE.Vector3()).subVectors(midpoint, globeInfo.pos)
   midpointVector = midpointVector.normalize()
   midpointVector = midpointVector.multiplyScalar(globeInfo.radius)
@@ -141,27 +179,174 @@ const makeMiniRegionFillerPoints = (lat, long) => {
   return [indices, points]
 }
 
-const triangleThing = (p1, p2, p3) => {
+const triangleThing = (p1Vec3, p2Vec3, p3Vec3) => {
+  // console.log(p1Vec3)
+  let v1 = (new THREE.Vector3()).subVectors(p2Vec3, p1Vec3)
+  let v2 = (new THREE.Vector3()).subVectors(p3Vec3, p2Vec3)
+  // console.log({ v1To2: v1To2, v2To3: v2To3 })
 
+  let points = []
+  // points.push(...p1Vec3)
+  for (let i = 0.0; i <= 1.0; i += 0.1) {
+    // let fractionV1 = v1.clone().multiplyScalar(0.8)
+    // let fractionV2 = v2.clone().multiplyScalar(1)
+    // let vCombination = (new THREE.Vector3()).addVectors(fractionV1, fractionV2)
+    // let newPoint = (new THREE.Vector3()).addVectors(p1Vec3, vCombination)
+
+    let fractionV1 = v1.clone().multiplyScalar(0.4)
+    for (let j = 0.0; j <= 1.0; j += 0.1) {
+      let fractionV2 = v2.clone().multiplyScalar(0.4 - (j / 2))
+      let vCombination = (new THREE.Vector3()).addVectors(fractionV1, fractionV2)
+      let newPoint = (new THREE.Vector3()).addVectors(p1Vec3, vCombination)
+      points.push(...newPoint)
+    }
+  }
+  // let fractionV1 = undefined
+  // let fractionV2 = undefined
+  // fractionV1 = v1.clone().multiplyScalar(0.9)
+  // fractionV2 = v2.clone().multiplyScalar(0.6)
+  // let vCombination = (new THREE.Vector3()).addVectors(fractionV1, fractionV2)
+  // let newPoint = (new THREE.Vector3()).addVectors(p1Vec3, vCombination)
+  // // let newPoint = (new THREE.Vector3()).addVectors(p1Vec3, fractionV1)
+  // // let newPoint = (new THREE.Vector3()).addVectors(p2Vec3, fractionV2)
+  // points.push(...newPoint)
+
+  return points
+
+  // TODO: if you can interpolate between a combination of 2x vectors, why can't you interpolate between a combination of 2x arcs?
 }
 
 const makeRegion = (latLongArr) => {
-  let longLatArr = latLongArr?.map(
-    (latLongJson) => {
-      return [latLongJson.long, latLongJson.lat]
-    }
-  )
+  const userLongLatArr = new Array(latLongArr.length)
+  const userPointsArr = new Array(latLongArr.length)
+  latLongArr.forEach((latLongJson, index) => {
+    userLongLatArr[index] = [latLongJson.long, latLongJson.lat]
 
-  let delaunay = d3Geo.geoDelaunay(longLatArr)
+    const [x, y, z] = ConvertLatLongToXYZ(latLongJson.lat, latLongJson.long, globeInfo.radius)
+    userPointsArr[index] = new THREE.Vector3(x, y, z)
+    // console.log(x, y, z)
+  })
+  // console.log({ userPointsArr: userPointsArr })
+
+  // let points = userPointsArr.map((vec3, index) => [...vec3]).flat()
+  let points = []
+  // console.log(points)
+
+  let delaunay = d3Geo.geoDelaunay(userLongLatArr)
+
   let indices = delaunay.triangles.flat()
 
-  let points = []
-  longLatArr.forEach((longLat) => {
-    let lat = longLat[1]
-    let long = longLat[0]
-    const [x, y, z] = ConvertLatLongToXYZ(lat, long, globeInfo.regionRadius)
-    points.push(x, y, z)
+  // delaunay.edges.forEach((startEndArr) => {
+  //   let startPointIndex = startEndArr[0]
+  //   let endPointIndex = startEndArr[1]
+  //   let startPoint = userPointsArr[startPointIndex]
+  //   let endPoint = userPointsArr[endPointIndex]
+  //   // console.log({ start: startPoint, end: endPoint })
+  //   let interpolatedPoints = interpolateArcFromVertices(startPoint, endPoint, 3)
+  //   let coordinates = interpolatedPoints.map((vec3, index) => [...vec3]).flat()
+  //   // console.log(coordinates)
+  //   points.push(...coordinates)
+  // })
+
+  // TODO: use this when iterating over the triangles to avoid doing the expensive interpolationArc calculation twice on the same edge
+  // Note: Inner edges always appear twice as the boundary between 2x triangles.
+  //??interpolate between _pairs_ of edges and only check those, rather than strictly triangles??
+  let completedEdges = {}
+  delaunay.edges.forEach((startEndArr) => {
+    let key = JSON.stringify(startEndArr)
+    completedEdges[key] = false
   })
+  delaunay.triangles.forEach((triangleIndicesArr, index) => {
+    if (index > 0) {
+      return
+    }
+
+    console.log(triangleIndicesArr)
+    let edge1 = [triangleIndicesArr[0], triangleIndicesArr[1]]
+    let edge2 = [triangleIndicesArr[1], triangleIndicesArr[2]]
+    let edge3 = [triangleIndicesArr[2], triangleIndicesArr[0]]
+
+    let p1 = userPointsArr[triangleIndicesArr[0]]
+    let p2 = userPointsArr[triangleIndicesArr[1]]
+    let p3 = userPointsArr[triangleIndicesArr[2]]
+    // console.log({ p1: p1, p2: p2, p3: p3 })
+    let interpolatedCoordinates = triangleThing(p1, p2, p3)
+    points.push(...interpolatedCoordinates)
+    // console.log({ things: things })
+  })
+
+  // console.log(edges)
+
+  // console.log({ userPoints: userPoints })
+
+
+
+  // // Delete triangles where the edges are not too long.
+  // let triangleIndices = {}
+  // delaunay.triangles.forEach((triangle, index) => {
+  //   triangleIndices[index] = triangle
+  // })
+
+  // let maxDist = 1
+  // let maxDistSqrd = maxDist * maxDist
+  // delaunay.edges.forEach((edge, index) => {
+  //   let pointsStartIndex = edge[0] * 3
+  //   let pointsEndIndex = edge[1] * 3
+
+  //   let p1 = new THREE.Vector3(
+  //     pointsArr[pointsStartIndex + 0],
+  //     pointsArr[pointsStartIndex + 1],
+  //     pointsArr[pointsStartIndex + 2])
+  //   let p2 = new THREE.Vector3(
+  //     pointsArr[pointsEndIndex + 0],
+  //     pointsArr[pointsEndIndex + 1],
+  //     pointsArr[pointsEndIndex + 2])
+  //   let v = (new THREE.Vector3).subVectors(p1, p2)
+  //   let distSqrd = v.dot(v)
+  //   if (distSqrd > maxDistSqrd) {
+  //     // Delete triangles with this edge.
+  //     delaunator.triangles.forEach((triangle, triangleIndex) => {
+  //       if (triangle.includes(edge[0]) && triangle.includes(edge[1])) {
+  //         delete triangleIndices[triangleIndex]
+  //       }
+  //     })
+  //   }
+  // })
+  // Object.keys(triangleIndices).forEach((key) => {
+  //   indicesArr.push(...(triangleIndices[key]))
+  // })
+
+
+
+
+  // // Interpolated region between points
+  // for (let i = 1; i < whereLatLongArr.length; i++) {
+  //   // Source:
+  //   //  https://en.wikipedia.org/wiki/Slerp
+  //   // Summary:
+  //   //  Convert latLong -> 3D vectors
+  //   //  Get unit vectors
+  //   //  Use dot product + acos(...) to get angle
+  //   //  Interpolate fraction from 0 -> 1
+  //   //    fractionStartVector = sin((1 - fraction) * angle) / sin(angle)
+  //   //    fractionEndVector = sin(fraction * angle) / sin(angle)
+  //   //    interpolatedVector = fractionStartVector * startVector + fractionEndVector * endVector
+  //   let startLat = whereLatLongArr[i].lat
+  //   let startLong = whereLatLongArr[i].long
+  //   let endLat = whereLatLongArr[i - 1].lat
+  //   let endLong = whereLatLongArr[i - 1].long
+  //   let interpolatedPoints = interpolateArc(startLat, startLong, endLat, endLong, 3)
+  //   interpolatedPoints.forEach((point) => {
+  //     fillerPointsArr.push(...point)
+  //   })
+  // }
+
+
+
+
+
+
+
 
   return [indices, points]
 }
@@ -173,7 +358,7 @@ export function Region({ latLongArr }) {
   const fillerPointsRef = useRef()
   const fillerPointsMeshRef = useRef()
   useEffect(() => {
-    console.log("MyPolygon -> useEffect -> regionMeshRef")
+    // console.log("Region -> useEffect -> regionMeshRef")
 
     let fillerPointsArr = []
     let fillerIndicesArr = []
@@ -189,13 +374,13 @@ export function Region({ latLongArr }) {
     if (copyLatlongArr.length > 1) {
       // console.log("more than 2 points")
 
-      const [midpointLat, midpointLong] = findMidpointOnSurface(whereLatLongArr)
-      copyLatlongArr.push({
-        lat: midpointLat,
-        long: midpointLong
-      })
+      // const [midpointLat, midpointLong] = findMidpointOnSurface(whereLatLongArr)
+      // copyLatlongArr.push({
+      //   lat: midpointLat,
+      //   long: midpointLong
+      // })
 
-      let thingPoint = whereLatLongArr[0]
+      // let thingPoint = whereLatLongArr[0]
 
 
       // Nope. 
@@ -236,28 +421,33 @@ export function Region({ latLongArr }) {
     fillerIndicesArr.push(...shiftedIndices)
     fillerPointsArr.push(...regionPoints)
 
+    const [midpointLat, midpointLong] = findMidpointOnSurface(whereLatLongArr)
+    const [x, y, z] = ConvertLatLongToXYZ(midpointLat, midpointLong, globeInfo.radius)
+    fillerPointsArr.push(x, y, z)
 
-    // Interpolated region between points
-    for (let i = 1; i < whereLatLongArr.length; i++) {
-      // Source:
-      //  https://en.wikipedia.org/wiki/Slerp
-      // Summary:
-      //  Convert latLong -> 3D vectors
-      //  Get unit vectors
-      //  Use dot product + acos(...) to get angle
-      //  Interpolate fraction from 0 -> 1
-      //    fractionStartVector = sin((1 - fraction) * angle) / sin(angle)
-      //    fractionEndVector = sin(fraction * angle) / sin(angle)
-      //    interpolatedVector = fractionStartVector * startVector + fractionEndVector * endVector
-      let startLat = whereLatLongArr[i].lat
-      let startLong = whereLatLongArr[i].long
-      let endLat = whereLatLongArr[i - 1].lat
-      let endLong = whereLatLongArr[i - 1].long
-      let interpolatedPoints = interpolateArc(startLat, startLong, endLat, endLong, 3)
-      interpolatedPoints.forEach((point) => {
-        fillerPointsArr.push(...point)
-      })
-    }
+
+
+    // // Interpolated region between points
+    // for (let i = 1; i < whereLatLongArr.length; i++) {
+    //   // Source:
+    //   //  https://en.wikipedia.org/wiki/Slerp
+    //   // Summary:
+    //   //  Convert latLong -> 3D vectors
+    //   //  Get unit vectors
+    //   //  Use dot product + acos(...) to get angle
+    //   //  Interpolate fraction from 0 -> 1
+    //   //    fractionStartVector = sin((1 - fraction) * angle) / sin(angle)
+    //   //    fractionEndVector = sin(fraction * angle) / sin(angle)
+    //   //    interpolatedVector = fractionStartVector * startVector + fractionEndVector * endVector
+    //   let startLat = whereLatLongArr[i].lat
+    //   let startLong = whereLatLongArr[i].long
+    //   let endLat = whereLatLongArr[i - 1].lat
+    //   let endLong = whereLatLongArr[i - 1].long
+    //   let interpolatedPoints = interpolateArc(startLat, startLong, endLat, endLong, 3)
+    //   interpolatedPoints.forEach((point) => {
+    //     fillerPointsArr.push(...point)
+    //   })
+    // }
 
     let valuesPerVertex = 3
     let valuesPerIndex = 1
@@ -267,7 +457,7 @@ export function Region({ latLongArr }) {
     let fillerPointsIndicesAttr = new THREE.Uint32BufferAttribute(fillerIndicesArr, valuesPerIndex)
 
     fillerPointsRef.current.geometry.setAttribute("position", fillerPointsPosAttr)
-    fillerPointsRef.current.geometry.attributes.position.needsUpdate = true
+    // fillerPointsRef.current.geometry.attributes.position.needsUpdate = true
 
     fillerPointsMeshRef.current.geometry.setAttribute("position", fillerPointsPosAttr)
     fillerPointsMeshRef.current.geometry.setIndex(fillerPointsIndicesAttr)
@@ -277,7 +467,7 @@ export function Region({ latLongArr }) {
   return (
     <>
       <points name="IntermediatePoints" ref={fillerPointsRef}>
-        <pointsMaterial color={0x00fff0} size={0.08} />
+        <pointsMaterial color={0x00fff0} size={0.18} />
       </points>
 
       <group name="LatLongPins">
@@ -300,7 +490,7 @@ export function Region({ latLongArr }) {
           save, change back to true, and save again.
       */}
       <mesh name="IntermediatePointsMesh" ref={fillerPointsMeshRef}>
-        <meshPhongMaterial attach="material" color={0xf0ff00} side={THREE.DoubleSide} wireframe={false} transparent={true} opacity={0.1} />
+        <meshPhongMaterial attach="material" color={0xf0ff00} side={THREE.DoubleSide} wireframe={true} transparent={true} opacity={0.1} />
       </mesh>
 
     </>
