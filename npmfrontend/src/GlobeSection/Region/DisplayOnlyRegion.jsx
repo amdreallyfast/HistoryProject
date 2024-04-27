4d2592d086096354470603a33fcb722bb8ffd7d6
amdreallyfast <amdreallyfast@hotmail.com>
2024-04-27 17:07:23 -0400

    TODO notes

--- npmfrontend/src/GlobeSection/Region/DisplayOnlyRegion.jsx
+++ npmfrontend/src/GlobeSection/Region/DisplayOnlyRegion.jsx
@@ -3,7 +3,11 @@ import { ConvertLatLongToVec3 } from "../convertLatLongXYZ"
 
 
 export function DisplayOnlyRegion({ poiId, lat, long, globeInfo }) {
-  // TODO: once I have an aray of searchable POIs, get rid of "lat, long" inputs, search the POIs for this ID, and extract the necessary data
+  // TODO:
+  //  once I have an array of searchable POIs, get rid of "lat, long" inputs, search the POIs for this ID, and extract the necessary data
+
+  // TODO:
+  //  Show 2x regions and compute intersection
 
 
   useEffect(() => {
