5de8b9ded5a1d2fec656529c229816a33b029896
amdreallyfast <amdreallyfast@hotmail.com>
2024-07-06 15:20:18 -0400

    Refactor: Rename "createWhere.jsx" -> "createSpherePoint.jsx"
    Makes more sense this way, I think.

--- npmfrontend/src/GlobeSection/createSpherePoint.jsx
+++ npmfrontend/src/GlobeSection/createSpherePoint.jsx
@@ -4,13 +4,13 @@ import { v4 as uuid } from "uuid"
 export function createSpherePointFromXYZ(x, y, z, sphereRadius) {
   // console.log({ msg: "createWhereFromXYZ", x, y, z })
   const [lat, long] = ConvertXYZToLatLong(x, y, z, sphereRadius)
-  let where = { id: uuid(), lat, long, x, y, z }
-  return where
+  let identifiedSpherePoint = { id: uuid(), lat, long, x, y, z }
+  return identifiedSpherePoint
 }
 
 export function createSpherePointFromLatLong(lat, long, sphereRadius) {
   // console.log({ msg: "createSpherePointFromLatLong", lat, long })
   const [x, y, z] = ConvertLatLongToXYZ(lat, long, sphereRadius)
-  let where = { id: uuid(), lat, long, x, y, z }
-  return where
+  let identifiedSpherePoint = { id: uuid(), lat, long, x, y, z }
+  return identifiedSpherePoint
 }
