e55d2cdde8b84d2c81fde9f35c523014b8899f17
amdreallyfast <amdreallyfast@hotmail.com>
2024-07-06 09:34:35 -0400

    Refactor: A bit more cleanup

--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -7,9 +7,22 @@ using System.Security.Cryptography.X509Certificates;
 using WebAPI.Exceptions;
 using WebAPI.Models;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Get cert that's needed to access the key vault.
+// Janky, but this is the way that I'm running the security during development. Only a machine
+// with the cert installed can even attempt to access the key vault.
+var certStore = new X509Store(StoreLocation.CurrentUser);
+certStore.Open(OpenFlags.ReadOnly);
+const string certCommonName = "historyprojectwebapp";
+var certsByName = certStore.Certificates.Find(X509FindType.FindBySubjectName, certCommonName, false);
+if (certsByName.Count == 0)
+{
+    throw new MissingKeyVaultAccessCertException(certCommonName);
+}
+var cert = certsByName.First();
+
+// Get the DB conn string from the key vault.
 const string tenantIdKey = "KeyVaultAccess:TenantId";
 const string appIdKey = "KeyVaultAccess:HistoryProjectWebAppId";
 const string vaultUriKey = "KeyVaultAccess:VaultUrl";
@@ -19,26 +32,6 @@ var tenantId = builder.Configuration.GetValue<String>(tenantIdKey) ?? throw new 
 var appId = builder.Configuration.GetValue<String>(appIdKey) ?? throw new MissingKeyVaultAccessConfigException(appIdKey);
 var vaultUri = builder.Configuration.GetValue<String>(vaultUriKey) ?? throw new MissingKeyVaultAccessConfigException(vaultUriKey);
 var secretName = builder.Configuration.GetValue<String>(secretNameKey) ?? throw new MissingKeyVaultAccessConfigException(secretNameKey);
-
-// Janky, but this is the way that I'm running the security during development. Only a machine
-// with the cert installed can even attempt to access the key vault.
-var certStore = new X509Store(StoreLocation.CurrentUser);
-certStore.Open(OpenFlags.ReadOnly);
-
-// Use "FindBySubjectName", which should perform a substring search of the subject name, not "FindBySubjectDistinguishedName", which will perform an exact string match of the subject name.
-// Source:
-//  https://learn.microsoft.com/en-us/dotnet/api/system.security.cryptography.x509certificates.x509certificate2.subjectname?view=net-8.0
-//const string certCommonName = "historyprojectwebapp";
-//var certsByName = certStore.Certificates.Find(X509FindType.FindBySubjectName, certCommonName, false);
-const string certCommonName = "CN=historyprojectwebapp, O=Internet Widgits Pty Ltd, S=Some-State, C=AU";    // Still, cool that I figured this out.
-var certsByName = certStore.Certificates.Find(X509FindType.FindBySubjectDistinguishedName, certCommonName, false);
-if (certsByName.Count == 0)
-{
-    throw new MissingKeyVaultAccessCertException(certCommonName);
-}
-var cert = certsByName.First();
-
-// Get the DB conn string from the key vault.
 var loginCredential = new ClientCertificateCredential(tenantId, appId, cert);
 var client = new SecretClient(new Uri(vaultUri), loginCredential);
 var response = await client.GetSecretAsync(secretName);
